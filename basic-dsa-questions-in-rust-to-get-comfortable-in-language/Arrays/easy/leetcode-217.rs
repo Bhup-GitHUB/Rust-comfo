60096b50fc4e5144bcdaf7b326fa9fd433da10ff
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-11-10 02:29:17 +0530

    leetvcode question 217 done with hashmap in rust

---
+++ basic-dsa-questions-in-rust-to-get-comfortable-in-language/Arrays/easy/leetcode-217.rs
@@ -0,0 +1,16 @@
+use std::collections::HashSet;
+
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut seen = HashSet::new();
+    for &num in &nums {
+        if !seen.insert(num) {
+            return true;
+        }
+    }
+    false
+}
+
+fn main() {
+    let nums = vec![1, 2, 3, 1];
+    println!("{}", contains_duplicate(nums)); // Output: true
+}
