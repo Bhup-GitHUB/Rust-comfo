412c2e94bd9a542f4a6de8ea6e0f1b353374a4f2
Bhupesh Kumar <bkumar_be23@thapar.edu>
2024-11-10 01:42:58 +0530

    solving problems

---
+++ basic-dsa-questions-in-rust-to-get-comfortable-in-language/Arrays/easy/captialization.rs
@@ -0,0 +1,17 @@
+fn capitalize_letter(s: &str) -> String {
+    s.split_whitespace()
+        .map(|word| {
+            let mut chars = word.chars();
+            match chars.next() {
+                Some(first) => first.to_uppercase().collect::<String>() + chars.as_str(),
+                None => String::new(),
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn main() {
+    let result = capitalize_letter("hello world");
+    println!("{}", result);
+}
